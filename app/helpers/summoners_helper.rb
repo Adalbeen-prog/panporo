d3a25ea687c17baad0c1dd588159c5c55a93ffb5
Adalbeen <admin@adalbeen.eu>
2020-03-31 16:15:20 +0200

    asd

--- app/helpers/summoners_helper.rb
+++ app/helpers/summoners_helper.rb
@@ -3,9 +3,13 @@ module SummonersHelper
     json.find {|h1| h1[1]['key'] == id.to_s }[1]
   end
 
-
   def define_owner_of_game(name, match_details)
     owner_id = match_details['participantIdentities'].find{ |x| x['player']['summonerName'] == name }['participantId']
     match_details['participants'][owner_id.to_i - 1]
+  end
+
+  def get_image_from_summoner_spell_id(json, id)
+    spell_name = get_name_from_id(json, id)['image']['full']
+    return image_tag("/10.6.1/img/spell/#{spell_name}", class: 'ui mini image')
   end
 end
