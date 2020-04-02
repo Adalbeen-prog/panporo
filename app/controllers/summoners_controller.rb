01c9a5be9562fc5506864e6d5a42a941dcc05766
Vojtech Prochazka <voj.proch@gmail.com>
2020-04-02 15:18:05 +0200

    add analysis

--- app/controllers/summoners_controller.rb
+++ app/controllers/summoners_controller.rb
@@ -1,7 +1,7 @@
 class SummonersController < ApplicationController
 
   def show
-    @api = ENV['RIOT_API'] ? ENV['RIOT_API'] : 'RGAPI-466ae6b3-1134-4f95-ac6e-7f904c858e75'
+    @api = ENV['RIOT_API'] ? ENV['RIOT_API'] : 'RGAPI-9b91504a-d78d-45f3-96ee-0c7be2d0858c'
     @region = 'eun1'
     @version = '10.7.1'
     @summonerinfo, @profile = get_profile('eun1', params[:summoner])
@@ -13,9 +13,11 @@ class SummonersController < ApplicationController
   end
 
   def match
+    @api = ENV['RIOT_API'] ? ENV['RIOT_API'] : 'RGAPI-9b91504a-d78d-45f3-96ee-0c7be2d0858c'
+    @region = 'eun1'
+    @version = '10.7.1'
     @champion_json = parse_http_json("http://ddragon.leagueoflegends.com/cdn/#{@version}/data/cs_CZ/champion.json")['data']
     @summoner_json = parse_http_json("http://ddragon.leagueoflegends.com/cdn/#{@version}/data/cs_CZ/summoner.json")['data']
-    @api = ENV['RIOT_API'] ? ENV['RIOT_API'] : 'RGAPI-466ae6b3-1134-4f95-ac6e-7f904c858e75'
-    @region = 'eun1'
+    @runes = parse_http_json("http://ddragon.leagueoflegends.com/cdn/#{@version}/data/cs_CZ/runesReforged.json")
   end
 end
--- app/helpers/summoners_helper.rb
+++ app/helpers/summoners_helper.rb
@@ -22,4 +22,50 @@ module SummonersHelper
     team2 = teams.select{ |x| x['teamId'] == 200 }
     return team1, team2
   end
+
+  def find_main_runes_by_id(id)
+    @runes.find { |x| x['id'] == id }
+  end
+
+  def largest_hash_key(hash1, hash2, thing)
+    a = hash1.max_by{|k| k['stats'][thing] }['stats'][thing]
+    b = hash2.max_by{|k| k['stats'][thing] }['stats'][thing]
+
+    return [a, b].sort[1]
+  end
+
+  def all_team_hash(hash, thing)
+    hash.map{|k| k['stats'][thing] }.inject(:+)
+  end
+
+  def count_stats(team1, team2)
+    max_in_team = {'0' => {}, '1' => {}}
+    all_team = {'0' => {}, '1' => {}}
+    [team1, team2].each_with_index do |team, i|
+      ['kills','goldEarned','totalDamageDealtToChampions', 'wardsPlaced', 'totalDamageTaken','totalMinionsTotal'].each do |stat|
+        max_in_team[i.to_s].merge!({stat => largest_hash_key(team1, team2, stat)})
+        all_team[i.to_s].merge!({stat => all_team_hash(team, stat)})
+      end
+    end
+    return max_in_team, all_team
+  end
+
+  def add_minions_and_monsters(team1, team2)
+    [team1, team2].each do |team|
+      team.each do |player|
+        player['stats'].merge!({'totalMinionsTotal' => player['stats']['totalMinionsKilled'] + player['stats']['neutralMinionsKilled']})
+      end
+    end
+  end
+
+  def make_percentage_from_max(value, max)
+    return ((value.to_f / max.to_f)*100).floor
+  end
+  
+  def team_ratio(all_team, stat)
+    counted = all_team['0'][stat] + all_team['1'][stat]
+    team1 = (all_team['0'][stat].to_f / counted.to_f * 100).round(2)
+    team2 = (all_team['1'][stat].to_f / counted.to_f * 100).round(2)
+    return [team1, team2]
+  end
 end
